1de7159b62906d3c81dc35ba711696a269bcec5b
sgtechiq <aahn87@proton.me>
2025-08-15 00:50:14 +0300

    Removed My Test Controllers

--- app/Events/ArticleDeleted.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Events;
-
-use Rivulet\Events\Event;
-
-class ArticleDeleted extends Event
-{
-    // Data passed: article id
-}
--- app/Helpers/slug.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function article_slug($name)
-{
-    return str_replace(' ', '-', strtolower($name));
-}
